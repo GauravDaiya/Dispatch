89286f6e84c1439f2f297e3faffc0d55d8f08376
Gaurav Khatri <gaurav.khatri@ajm.in>
2025-03-04 18:20:47 +0530

    final

--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -15,41 +15,41 @@ export class CustomerService {
    *=============== Get Single Customer ==========================
    */
   public getCustomerReadByIdService(customerId: any) {
-    return this.http.get(`https://gauravdaiya.github.io/DispatchDb/db.json/customer/${customerId}`)
+    return this.http.get(`https://json-server-app-u17t.onrender.com/customer/${customerId}`)
   }
 
   /**
    *=============== Add Customers ==========================
    */
   public createCustomer(customerData: any) {
-    return this.http.post('https://gauravdaiya.github.io/DispatchDb/db.json/customer', customerData)
+    return this.http.post('https://json-server-app-u17t.onrender.com/customer', customerData)
   }
 
   /**
  *=============== Get All Customers List ==========================
  */
   public getCustomers(): Observable<any []> {
-    return this.http.get<any []>('https://gauravdaiya.github.io/DispatchDb/db.json/customer')
+    return this.http.get<any []>('https://json-server-app-u17t.onrender.com/customer')
   }
 
    /**
  *=============== Search Customers ==========================
  */
  public searchCustomers(SearchString:string) {
-  return this.http.get(`https://gauravdaiya.github.io/DispatchDb/db.json/customer?firstName=${SearchString}`)
+  return this.http.get(`https://json-server-app-u17t.onrender.com/customer?firstName=${SearchString}`)
 }
 
   /**
  *=============== Edit Customer ==========================
  */
   public editCustomer(customerId: any, updatedCustomerData: any) {
-    return this.http.put(`https://gauravdaiya.github.io/DispatchDb/db.json/customer/${customerId}`, updatedCustomerData)
+    return this.http.put(`https://json-server-app-u17t.onrender.com/customer/${customerId}`, updatedCustomerData)
   }
 
   /**
     *=============== Remove Customer ==========================
     */
   public DeleteCustomerService(DelId: any) {
-    return this.http.delete(`https://gauravdaiya.github.io/DispatchDb/db.json/customer/${DelId}`)
+    return this.http.delete(`https://json-server-app-u17t.onrender.com/customer/${DelId}`)
   }
 }
